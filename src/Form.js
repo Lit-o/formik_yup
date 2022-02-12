71dc7e285b1d8d1f060117108fc5769a14bfb920
Boris <boris12lit@gmail.com>
2022-02-12 17:50:45 +0300

    change h2

--- src/Form.js
+++ src/Form.js
@@ -53,7 +53,7 @@ const Form = () => {
 
     return (
         <form className="form" onSubmit={formik.handleSubmit}>
-            <h2>Отправить пожертвование</h2>
+            <h2>Отправить деньги</h2>
             <label htmlFor="name">Ваше имя</label>
             <input
                 id="name"
